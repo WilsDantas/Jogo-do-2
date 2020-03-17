e93e995f89cadf19384221ca6276649f79f8b8f9
WilsDantas <wilsdantas@gmail.com>
2020-03-17 09:30:44 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var letterSelected = 0
+var playerActive = 1
+
+var playerOne = {
+    points: 0
+}
+var playerTwo = {
+    points: 0
+}
+
+
+var player1D = document.getElementById('player1')
+var player2D = document.getElementById('player2')
+
+var scores1D = document.getElementById('scores1')
+var scores2D = document.getElementById('scores2')
+
+
+var letterSelectedD = document.getElementById('letter-selected')
